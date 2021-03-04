77bfc3a659dafd2823ceb2f43127b8876d013bc1
Stas ProSky <prosky25@gmail.com>
2021-03-04 21:47:26 +0300

    - 1st lecture

--- src/main/kotlin/day_1_kotlin/langrus/assignments/KotlinWorkshop2.kt
+++ src/main/kotlin/day_1_kotlin/langrus/assignments/KotlinWorkshop2.kt
@@ -41,8 +41,13 @@ object KotlinWorkshop2 {
             print("Введите число в диапазоне 0..$nonNullUpperBound включительно: ")
             // Сохраняем введённое с клавиатуры число в "userInput".
             val userInput: Int = scanner.nextInt()
-            if (true) {
-
+            if (userInput == randomNumber) {
+                print("Поздравляю! Задуманное число $randomNumber")
+                break
+            } else if (userInput  > randomNumber) {
+                print("Ваше число больше задуманного, пожалуйста продолжаем.")
+            } else {
+                print("Ваше число меньше задуманного, пожалуйста продолжаем.")
             }
         }
 
@@ -51,25 +56,27 @@ object KotlinWorkshop2 {
 
         // TODO 2: Раскомментируй. Проинициализируй "numbersArray".
         //  "numbersArray" это массив целочисленных значений, длинной 5 чисел.
-//        val numbersArray =
+        val numbersArray = arrayOfNulls<Int>(5)
 
         // TODO 3: Раскомментируй. Проинициализируй свойство "size" длинной массива "numbersArray".
         //  Измени условия повтора бесконечного цикла while так, чтобы он стал конечным и выполнился не более "size" раз.
         println("\n Программа 2. \"Введите коллекцию чисел\"")
-//        val size =
+        val size = numbersArray.size
         var counter = 0
-        while (true) {
+        while (counter < size) {
             print("Введите число в диапазоне 0..10 включительно: ")
             val userInput: Int = scanner.nextInt()
 
-            // TODO 4: Сохрани и добавь введённые с клавиатуры числа в массив "numbersArray" согласно следующей логики:
+            // TODO 4: Сохрани и добавь введённые с клавиатуры числа в массив "numbersArray" согласно следующей логике:
             //  - Если введено 3 - добавь число в массив без изменений;
             //  - Если введено 5 - умнож и добавь (5 * size);
             //  - Если введено 9 - сложи и добавь (2 + size);
             //  - Иначе не добавляй ничего.
             //  Используй выражение "When".
-            when {
-
+            when (userInput) {
+                3 -> numbersArray[counter] = userInput
+                5 -> numbersArray[counter] = userInput.times(size)
+                9 -> numbersArray[counter] = userInput.plus(size)
             }
 
             counter++
@@ -77,7 +84,9 @@ object KotlinWorkshop2 {
 
         // TODO 5: Выведи в консоль значения элементов массива и индекс, на котором они находятся.
         //  Новый элемент на новой строке. Это должно выглядеть как "индекс: значение".
-
+        for ((index, value) in numbersArray.withIndex()) {
+            println("$index: $value")
+        }
     }
 
 
